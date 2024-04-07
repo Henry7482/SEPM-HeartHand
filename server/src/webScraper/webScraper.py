4054149cd60e6810e19b9fdaa923e19a3c83952e
Tran Phan Trong Phuc <117657788+Henry7482@users.noreply.github.com>
2024-04-07 20:45:55 +0700

    HEART-16 implemented web scraper for 1 website

--- server/src/webScraper/webScraper.py
+++ server/src/webScraper/webScraper.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 
 # Function scrape data tu website (demo)
 def scrape_news():
-    url = 'https://dantri.com.vn/tim-kiem/bao+vietnamnet.htm'
+    url = 'https://e.vnexpress.net/news/news/environment'
 
     response = requests.get(url)
 
@@ -13,21 +13,43 @@ def scrape_news():
 
         soup = BeautifulSoup(response.content, 'html.parser')
 
-        articles = soup.find_all('___', class_='___') # Chua biet team frontend se dung id/class ntn
+        articles_headers = soup.find_all('h2', class_='title_news_site', limit=5) 
+
+        # articles_links = [article.find('a')['href'] for article in articles_headers]
+        articles_links = ["https://e.vnexpress.net/news/education/vietnam-approves-ielts-one-skill-retake-4730846.html"]
+
+        articleDic = {}
+        articles = []
+        for link in articles_links:
+            response = requests.get(link)
+            if response.status_code == 200:
+                article = BeautifulSoup(response.content, 'html.parser')
+                articleDic["data"] = article
+                articleDic["link"] = link
+                articles.append(articleDic)
+            else:
+                print('Failed to retrieve data from a website.')
+                continue
 
         # Vdu store data vao database
         scraped_data = []
 
         # Extract info tu cac bai bao
         for article in articles:
-            title = article.find('h2', class_='article-title').text.strip()
-            author = article.find('p', class_='author').text.strip()
-            request_donation = article.find('span', class_='donation').text.strip()
+
+            title = article.get("data").find('h1', class_='title_post').text.strip()
+
+            content = article.get("data").find('span', class_='lead_post_detail row').text.strip()
+            for p in article.get("data").find_all('p', class_='Normal'):
+                content += "\n" + p.text.strip()
+
+            publish_date = article.get("data").find('div', class_='author').text.strip()
+            publish_date = publish_date.split('&nbsp')[1]
 
             data = {
                 'title': title,
-                'author': author,
-                'donation': request_donation
+                'content': content,
+                'publish_date': publish_date
             }
 
             # Them dictionary data vao db
@@ -37,6 +59,16 @@ def scrape_news():
     else:
         print('Failed to retrieve data from the website.')
         return None
+
+
+# def tester():
+#     response = requests.get('https://e.vnexpress.net/news/education/vietnam-approves-ielts-one-skill-retake-4730846.html')
+#     soup = BeautifulSoup(response.content, 'html.parser')
+#     publish_date = soup.find('div', class_='author').text.strip()
+#     publish_date = publish_date.split('&nbsp')[1]
+#     print(publish_date)
+
+# tester()
 
 data = scrape_news()
 for d in data:
