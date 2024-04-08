7fdd47cf5d263a94691b605a79814390559b33bc
Tran Phan Trong Phuc <117657788+Henry7482@users.noreply.github.com>
2024-04-08 23:49:49 +0700

    added validation for scraperdata create route

--- server/src/controllers/scraperData/createScraperData.js
+++ server/src/controllers/scraperData/createScraperData.js
@@ -3,6 +3,10 @@ import ScraperData from "../../models/ScaperData.js";
 const createScraperData = async (req, res) => {
   try {
     const scraperData = req.body;
+    if (!scraperData || typeof scraperData !== "object") {
+      return res.status(400).json({ message: "Input is not a valid JSON object" });
+    }
+
     const output = await ScraperData.create(scraperData);
     res.status(200).json({ output });
   } catch (error) {
