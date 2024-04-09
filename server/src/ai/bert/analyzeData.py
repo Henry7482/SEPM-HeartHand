62a22ebc6453c6d7495fd096e6a7409e331ad404
nguyenhungvinh <s3778972@rmit.edu.vn>
2024-04-10 02:59:25 +0700

    bert model imported (still need further discussion)
    a đang ko test đc cái bertopic do cái terminal a nó bị cái gì ấy. hiện tại a có thử theo video vs 1 số nguồn tutorial thì thử viết nó ra như này. e xóa comments r xem thử nó có work ko nhé. a để có mấy cái lệnh để test case ấy

--- server/src/ai/bert/analyzeData.py
+++ server/src/ai/bert/analyzeData.py
@@ -1,3 +1,32 @@
-from bretopic import BERTopic
+from bertopic import BERTopic 
 import json
 import pandas as pd
+
+# model = BERTopic()
+
+# # Read text data from path
+# file_path = "bert/message.json" 
+# documents = []
+# with open(file_path, 'r') as file:
+#     data = json.load(file)
+#     for item in data:
+#         documents.append(item["text"])
+
+# topics, _ = model.fit_transform(documents)
+# top_topics = model.get_topic_info()
+
+# print("Top Topics:")
+#print(top_topics)
+
+
+with open("BERT/message.txt", "r", encoding = "utf-8") as f:
+    docs = json.load(f)
+titles = [article["title"] for article in docs]
+# len(docs)
+# for title in titles:
+#   print(title)
+topic_model = BERTopic(embedding_model = "all-MiniLM-L6-v2")
+topics, probs = topic_model.fit_transform(docs)
+# topic_model.get_topic_info()
+df = pd.DataFrame({"topic": topics, "document": docs})
+# df
