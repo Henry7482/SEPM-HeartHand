d43afe76da7eee71182c0fb79ed88679546d011d
Tran Phan Trong Phuc <117657788+Henry7482@users.noreply.github.com>
2024-04-11 14:51:46 +0700

    HEART-6 added homepage file

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-import Footer from './footer/footer.tsx';
+import "./App.css";
+import Footer from "./footer/footer.tsx";
+import Header from "./header/header.jsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Body from "./body/body.tsx";
+import HomePage from "./homePage/homePage.tsx";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-        <BrowserRouter>
-      <Routes>
-        <Route path="/footerTest" element={<Footer />} />
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/footerTest" element={<Footer />} />
+          <Route path="/bodyTest" element={<Body />} />
+          <Route path="/headerTest" element={<Header />} />
+          <Route path="/homeTest" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ client/src/homePage/homePage.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from '../header/header';
+import Body from '../body/body.tsx';
+import Footer from '../footer/footer.tsx';
+
+const HomePage = () => {
+    return (
+    <div className="home-page">
+        <Header />
+        <Body />
+        <Footer />
+    </div>
+    );
+}
+
+export default HomePage;
