73df6603a01ab34a4df0d13f77d725cab976a8f1
Tran Phan Trong Phuc <117657788+Henry7482@users.noreply.github.com>
2024-04-11 15:10:09 +0700

    HEART-10 fixed homepage files

--- client/src/homePage/homePage.tsx
+++ client/src/homePage/homePage.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from '../header/header';
+import Header from '../header/header.jsx';
 import Body from '../body/body.tsx';
 import Footer from '../footer/footer.tsx';
 
