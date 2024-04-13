1e8c0b6e4429b1344bb94d7ef2127d62ac6636c0
Truongphamvinhkhang123 <131866195+Truongphamvinhkhang123@users.noreply.github.com>
2024-04-13 16:18:45 +0700

    Update body.tsx

--- client/src/body/body.tsx
+++ client/src/body/body.tsx
@@ -1,40 +1,37 @@
 import React from "react";
 import "./body.css";
+import Homepage from "../assets/homepage.jpeg"
 
 const Body = () => {
   return (
+
     <div className="body-container">
       <div className="page-name">
         <h2>Latest News</h2>
         <p className="italic-line">Get the perspective and insights that improve the lives for people in our nation</p>
       </div>
       <div className="home-page"> 
-        <img src="picture1.png" alt="Home" className="home-picture" /> 
+        <img src="homepage.jpeg" alt="Home" className="home-picture" /> 
       </div>
       <div className="blog-detail">
         <img src="news-picture.jpg" alt="Home" className="news-picture" />
         <div className="blog-content">
           <h2>Mưa lớn gây lũ lụt nghiêm trọng tại Sydney, Australia</h2>
-          <p>VTV.vn - Khu vực Đông Nam Australia đã phải hứng chịu mưa lớn từ ngày 4/4 và sẽ tiếp tục kéo dài đến sáng 6/4 (theo giờ địa phương), gây ngập lụt trên diện rộng...</p>
+          <li><i>VTV.vn - Khu vực Đông Nam Australia đã phải hứng chịu mưa lớn từ ngày 4/4 và sẽ tiếp tục kéo dài đến sáng 6/4 (theo giờ địa phương), gây ngập lụt trên diện rộng...</i></li>
         </div>
       </div>
       <div className="news-picture">
         <img src="news-picture.jpg" alt="Home" className="news-picture" />
         <div className="blog-content">
           <h2>Mưa lớn gây lũ lụt nghiêm trọng tại Sydney, Australia</h2>
-          <p>VTV.vn - Khu vực Đông Nam Australia đã phải hứng chịu mưa lớn từ ngày 4/4 và sẽ tiếp tục kéo dài đến sáng 6/4 (theo giờ địa phương), gây ngập lụt trên diện rộng...</p>
+          <li><i>VTV.vn - Khu vực Đông Nam Australia đã phải hứng chịu mưa lớn từ ngày 4/4 và sẽ tiếp tục kéo dài đến sáng 6/4 (theo giờ địa phương), gây ngập lụt trên diện rộng...</i></li>
         </div>
       </div>
       <div className="blog-detail">
         <img src="news-picture.jpg" alt="Home" className="news-picture" />
         <div className="blog-content">
           <h2>Mưa lớn gây lũ lụt nghiêm trọng tại Sydney, Australia</h2>
-          <p>VTV.vn - Khu vực Đông Nam Australia đã phải hứng chịu mưa lớn từ ngày 4/4 và sẽ tiếp tục kéo dài đến sáng 6/4 (theo giờ địa phương), gây ngập lụt trên diện rộng...</p>
+          <li><i>VTV.vn - Khu vực Đông Nam Australia đã phải hứng chịu mưa lớn từ ngày 4/4 và sẽ tiếp tục kéo dài đến sáng 6/4 (theo giờ địa phương), gây ngập lụt trên diện rộng...</i></li>
         </div>
       </div>
     </div>
-  );
-};
-
-export default Body;
-
