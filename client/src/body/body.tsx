63d58a3bf25a01a3bda57ae3b81f1f0195decd97
Truongphamvinhkhang123 <131866195+Truongphamvinhkhang123@users.noreply.github.com>
2024-04-13 16:26:13 +0700

    Update body.tsx

--- client/src/body/body.tsx
+++ client/src/body/body.tsx
@@ -17,21 +17,24 @@ const Body = () => {
         <img src="news-picture.jpg" alt="Home" className="news-picture" />
         <div className="blog-content">
           <h2>Mưa lớn gây lũ lụt nghiêm trọng tại Sydney, Australia</h2>
-          <p>VTV.vn - Khu vực Đông Nam Australia đã phải hứng chịu mưa lớn từ ngày 4/4 và sẽ tiếp tục kéo dài đến sáng 6/4 (theo giờ địa phương), gây ngập lụt trên diện rộng...</p>
+          <li><i>VTV.vn - Khu vực Đông Nam Australia đã phải hứng chịu mưa lớn từ ngày 4/4 và sẽ tiếp tục kéo dài đến sáng 6/4 (theo giờ địa phương), gây ngập lụt trên diện rộng...</i></li>
+          <p><i>28 March 2024 | Article</i></p>
         </div>
       </div>
       <div className="news-picture">
         <img src="news-picture.jpg" alt="Home" className="news-picture" />
         <div className="blog-content">
           <h2>Mưa lớn gây lũ lụt nghiêm trọng tại Sydney, Australia</h2>
-          <p>VTV.vn - Khu vực Đông Nam Australia đã phải hứng chịu mưa lớn từ ngày 4/4 và sẽ tiếp tục kéo dài đến sáng 6/4 (theo giờ địa phương), gây ngập lụt trên diện rộng...</p>
+          <li><i>VTV.vn - Khu vực Đông Nam Australia đã phải hứng chịu mưa lớn từ ngày 4/4 và sẽ tiếp tục kéo dài đến sáng 6/4 (theo giờ địa phương), gây ngập lụt trên diện rộng...</i></li>
+          <p><i>28 March 2024 | Article</i></p>
         </div>
       </div>
       <div className="blog-detail">
         <img src="news-picture.jpg" alt="Home" className="news-picture" />
         <div className="blog-content">
           <h2>Mưa lớn gây lũ lụt nghiêm trọng tại Sydney, Australia</h2>
-          <p>VTV.vn - Khu vực Đông Nam Australia đã phải hứng chịu mưa lớn từ ngày 4/4 và sẽ tiếp tục kéo dài đến sáng 6/4 (theo giờ địa phương), gây ngập lụt trên diện rộng...</p>
+          <li><i>VTV.vn - Khu vực Đông Nam Australia đã phải hứng chịu mưa lớn từ ngày 4/4 và sẽ tiếp tục kéo dài đến sáng 6/4 (theo giờ địa phương), gây ngập lụt trên diện rộng...</i></li>
+          <p><i>28 March 2024 | Article</i></p>
         </div>
       </div>
     </div>
@@ -39,5 +42,4 @@ const Body = () => {
 };
 
 export default Body;
-
 
