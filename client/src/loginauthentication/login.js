a367cf4f7cca35d346a7b748b63faf665001d5c3
Truongphamvinhkhang123 <131866195+Truongphamvinhkhang123@users.noreply.github.com>
2024-04-19 14:06:57 +0700

    update

--- client/src/loginauthentication/login.js
+++ client/src/loginauthentication/login.js
@@ -1,40 +1,18 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
-import React, { Component } from "react";
-import "./login.css";
+import React from "react";
+import './login.css'
 
-const Login = () => {
-    const[username,usernameupdate]=useState('');
-    const[password,passwordupdate]=useState('');
+import user_icon from '../assets/usericon.png'
+import email_icon from '../assets/email.png'
+import password_icon from '../assets/password.png'
 
-    const ProceedLogin=(e)=>{
-        e.preventDefault();      
-    }
-    return (
-        <div className="row">
-            <div className="offset-lg-3 col-lg-6">
-                <form onSubmit={ProceedLogin} className="container">
-                    <div className="card">
-                        <div className="card-header">
-                             <h2>User Login</h2>
-                        </div>
-                        <div className="card-body">
-                            <div className="form-group">
-                            <label>User Name<span className="errmsg">*</span></label>
-                                <input className="form-control"></input>
-                            </div>
-                            <div className="form-group">
-                            <label>Password<span className="errmsg">*</span></label>
-                                <input className="form-control"></input>
-                            </div>
-                        </div>
-                        <div className="card-footer">
-                            <button type="submit" className="btn btn-primary">Login</button> |
-                             <Link className="btn btn-success" to={'/register'}>New User</Link>
-                        </div>                       
-                    </div>
-                </form>
+const login = () => {
+    return(
+        <div className="container">
+            <div className="header">
+                
             </div>
-        </div>       
-    );
+        </div>
+    )
 }
+
+export default login
