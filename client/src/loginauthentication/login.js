ae25919287a54f35b98952f9e5398c97f4adf6df
Truongphamvinhkhang123 <131866195+Truongphamvinhkhang123@users.noreply.github.com>
2024-04-19 13:28:42 +0700

    update

--- client/src/loginauthentication/login.js
+++ client/src/loginauthentication/login.js
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
+import React, { Component } from "react";
+import "./login.css";
 
 const Login = () => {
     const[username,usernameupdate]=useState('');
@@ -20,7 +23,15 @@ const Login = () => {
                             <label>User Name<span className="errmsg">*</span></label>
                                 <input className="form-control"></input>
                             </div>
+                            <div className="form-group">
+                            <label>Password<span className="errmsg">*</span></label>
+                                <input className="form-control"></input>
+                            </div>
                         </div>
+                        <div className="card-footer">
+                            <button type="submit" className="btn btn-primary">Login</button> |
+                             <Link className="btn btn-success" to={'/register'}>New User</Link>
+                        </div>                       
                     </div>
                 </form>
             </div>
