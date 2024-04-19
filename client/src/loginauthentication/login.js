01be88e202978ea757b60e8a8ac04fd4ac6f3b0f
Truongphamvinhkhang123 <131866195+Truongphamvinhkhang123@users.noreply.github.com>
2024-04-19 15:55:19 +0700

    Update login.js

--- client/src/loginauthentication/login.js
+++ client/src/loginauthentication/login.js
@@ -26,7 +26,7 @@ const login = () => {
                     <input type="password" />
                 </div>
             </div>
-            <div className="forgot-password">Loss Password <span>Click Here!</span></div>
+            <div className="forgot-password">Loss Password? <span>Click Here!</span></div>
             <div className="submit-container">
                 <div className="submit">Sign Up</div>
                 <div className="submit">Login</div>
