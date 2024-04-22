e4b44a4157cb492b148c2bbc1e191e230867570f
Tran Phan Trong Phuc <117657788+Henry7482@users.noreply.github.com>
2024-04-22 11:11:11 +0700

    cleaning caches

--- server/flask/webScraper/tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-# Read data from JSON file
-with open('./server/flask/webScraper/data.json', 'r') as file:
-    data = json.load(file)
-
-# Print out the "title" attribute
-for article in data['data']:
-    print(article['title'])
