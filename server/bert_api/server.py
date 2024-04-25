e074cf49dd54229b61413180a57b2410808f2e53
Tran Phan Trong Phuc <117657788+Henry7482@users.noreply.github.com>
2024-04-25 23:52:43 +0700

    new app config

--- server/bert_api/server.py
+++ server/bert_api/server.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from .analyzeData import analyzeData
 from pydantic import BaseModel
+import os
+import uvicorn
 
 class Data(BaseModel):
     data: list
@@ -15,3 +17,7 @@ async def read_root():
 async def predict(data: Data):
     prompts = await analyzeData(data)
     return prompts
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run(app, host='0.0.0.0', port=port, debug=True)
