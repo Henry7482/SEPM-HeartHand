7608aa2d53a62074911f65e3a946e47ddc305674
Tran Phan Trong Phuc <117657788+Henry7482@users.noreply.github.com>
2024-04-26 12:19:16 +0700

    update bert_api dockerfile

--- server/bert_api/server.py
+++ server/bert_api/server.py
@@ -20,4 +20,4 @@ async def predict(data: Data):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
-    uvicorn.run(app, host='0.0.0.0', port=port, debug=True)
+    uvicorn.run(app, host='0.0.0.0', port=port, log_level="info")
