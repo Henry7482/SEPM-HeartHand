5a481d8441b73b31e13fe6748839d07d8055e675
Tran Phan Trong Phuc <117657788+Henry7482@users.noreply.github.com>
2024-04-27 12:25:41 +0700

    remove bert from flask app

--- server/flask/bert/functions/keywords_extraction.py
+++
@@ -1,9 +0,0 @@
-def extract_keywords(loaded_model, topic_id, top_n=10):
-    # Get the representation of the topic
-    topic_representation = loaded_model.get_topic(topic_id)
-    if topic_representation:
-        # Extract top keywords from the topic representation
-        topic_keywords = [topic_representation[i][0] for i, keyword in enumerate(topic_representation)]
-        return topic_keywords
-    else:
-        return []  # Return an empty list if no representation found for the topic
