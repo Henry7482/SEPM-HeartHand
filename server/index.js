eacf911c88a9d3c1892cd1b4ef0fb57b2cf6f1de
Tran Phan Trong Phuc <117657788+Henry7482@users.noreply.github.com>
2024-05-14 00:14:59 +0700

    HEART-26 completed delivery functions

--- server/index.js
+++ server/index.js
@@ -43,7 +43,7 @@ const start = async () => {
   try {
     await connectDB();
     // console.log("No Mongo")
-    app.listen(8080, () => {
+    app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
   } catch (err) {
--- server/src/routes/deliveryRoutes.js
+++ server/src/routes/deliveryRoutes.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { createDeliveryOrder } from "../controllers/deliveryControllers.js";
+import { createDeliveryOrder, cancelDeliveryOrder } from "../controllers/deliveryControllers.js";
+import { jwtAuthDonor } from "../middlewares/cookiejwtAuth.js";
 
 const deliveryRouter = Router();
 
+deliveryRouter.use(jwtAuthDonor);
+
 deliveryRouter.route("/create").post(createDeliveryOrder);
+deliveryRouter.route("/cancel").delete(cancelDeliveryOrder);
 
 export default deliveryRouter;
