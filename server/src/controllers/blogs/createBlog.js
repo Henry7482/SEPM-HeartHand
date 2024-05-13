6a617456564bf5b206db3fb0ee5b569ea4ef7588
Tran Phan Trong Phuc <117657788+Henry7482@users.noreply.github.com>
2024-05-13 23:03:31 +0700

    update

--- server/src/controllers/blogs/createBlog.js
+++ server/src/controllers/blogs/createBlog.js
@@ -1,30 +1,18 @@
-import Blog from "../../models/Blogs.js";
-import genrerateBLog from "../../ai/gpt/generateBlog.js";
+import Blog from "../../models/Blog.js";
 import uploadImagesToCloudinary from "../../models/uploadImages.js";
-
-const newBlogInstance = async () => {
-  const keywords = ["test", "blog"];
-  const references = ["test", "blog"];
-  const imageURL = null;
-  const defaultImageURL = "https://www.defaultURL.com";
-  const rawBlogs = await genrerateBLog(keywords);
-
-  const blog = new Blog({
-    title: rawBlogs.title,
-    shortform: rawBlogs.shortform,
-    content: rawBlogs.content,
-    date: new Date(),
-    keywords: keywords,
-    references: references,
-    imageURL: imageURL || defaultImageURL,
-  });
-  return blog;
-};
 
 const createBlog = async (req, res) => {
   try {
-    const imageUrls = await uploadImagesToCloudinary('../../images');
-    const blog = await newBlogInstance();
+    const imageURL = await uploadImagesToCloudinary('file image bỏ vào đây');
+    const blog = new Blog({
+      title: req.title, 
+      shortform: req.shortform,
+      content: req.content,
+      date: new Date(),
+      keywords: req.keywords,
+      references: req.references,
+      imageURL: imageURL || defaultImageURL,
+    });
     const output = await Blog.create(blog);
     res.status(200).json({message: "Successfully added blog", output });
   } catch (error) {
