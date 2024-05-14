a58d198e91aa608a91308e6a05d9db862e012f97
Tran Phan Trong Phuc <117657788+Henry7482@users.noreply.github.com>
2024-05-14 15:15:03 +0700

    changed port for testing

--- server/index.js
+++ server/index.js
@@ -45,7 +45,7 @@ const start = async () => {
   try {
     await connectDB();
     // console.log("No Mongo")
-    app.listen(PORT, () => {
+    app.listen(8080, () => {
       console.log(`Server is running on port ${PORT}`);
     });
   } catch (err) {
