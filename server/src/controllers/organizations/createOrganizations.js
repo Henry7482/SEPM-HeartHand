e9b0a0b59081902bdd47c63022cabda73286f80d
Tran Phan Trong Phuc <117657788+Henry7482@users.noreply.github.com>
2024-05-17 06:08:28 +0700

    another fix

--- server/src/controllers/organizations/createOrganizations.js
+++ server/src/controllers/organizations/createOrganizations.js
@@ -1,25 +1,35 @@
 import Organization from "../../models/Organization.js";
-import uploadImageToCloudinary from "../../utils/uploadImageToCloudinary.js";
+import { uploadImageToCloudinary } from "../../services/uploadImages.js";
 
 const createOrganizations = async (req, res) => {
-
   try {
     const file = req.file;
     if (!file) {
-      console.log('No file uploaded.');
-      req.body.imageURL = '';
+      console.log("No file uploaded.");
+      req.body.imageURL = "";
     }
 
     const imageUrl = await uploadImageToCloudinary(file.buffer);
     // console.log('Image URL:', imageUrl);
     req.body.imageURL = imageUrl;
   } catch (error) {
-    console.error('Error uploading image:', error);
-    req.body.imageURL = '';
+    console.error("Error uploading image:", error);
+    req.body.imageURL = "";
   }
 
   try {
-    const { name, phone_number, address, ward, district, province, email, website, keywords, imageURL } = req.body;
+    const {
+      name,
+      phone_number,
+      address,
+      ward,
+      district,
+      province,
+      email,
+      website,
+      keywords,
+      imageURL,
+    } = req.body;
     const organization = new Organization({
       name,
       phone_number,
