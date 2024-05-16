e9ad00498d3d3e76add89e34aec46de1a215b76a
Tran Phan Trong Phuc <117657788+Henry7482@users.noreply.github.com>
2024-05-16 10:52:00 +0700

    add deleting blogs

--- server/src/controllers/blogsControllers.js
+++ server/src/controllers/blogsControllers.js
@@ -3,8 +3,7 @@ import {getGeneratedBlogsById} from "./blogs/generatedBlogs.js"
 import createBlog from "./blogs/createBlog.js";
 import updateBlog from './blogs/updateBlog.js';
 import deleteBlog from './blogs/deleteBlog.js';
-import {getGeneratedBlogs, uploadGeneratedBlogs} from './blogs/generatedBlogs.js';
-// import {uploadImageToCloudinary} from './blogs/uploadImageToCloudinary.js';
-import {updateBlogWithImageUrl} from './blogs/updateBlogWithImageUrl.js';
+import {getGeneratedBlogs, uploadGeneratedBlogs, deleteGeneratedBlogs} from './blogs/generatedBlogs.js';
 
-export { getBlogs, createBlog, getBlogbyId, updateBlog, deleteBlog, getGeneratedBlogs, getGeneratedBlogsById, uploadGeneratedBlogs, updateBlogWithImageUrl } 
+
+export { getBlogs , createBlog, getBlogbyId, updateBlog, deleteBlog, getGeneratedBlogs, uploadGeneratedBlogs, deleteGeneratedBlogs} 
